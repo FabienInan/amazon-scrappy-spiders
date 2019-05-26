e11c489aaf68b034dd3fdafae004bcd6c0bbf713
Fabien Inan <fabien.inan@gmail.com>
2019-05-26 15:38:17 -0700

    update

---
+++ skeleton_app/amazonReviewsSpider.py
@@ -0,0 +1,33 @@
+import scrapy
+
+class AmazonReviewsSpider(scrapy.Spider):
+    
+    name = "amazonReviewsSpider"
+
+    def __init__(self, *args, **kwargs):
+        id = kwargs.pop('id', []) 
+        if id:
+            url = "https://www.amazon.ca/product-reviews/" + str(id) + "/?ie=UTF8&reviewerType=all_reviews&pageNumber="
+            self.start_urls=[]
+            for i in range(1,100):
+                self.start_urls.append(url + str(i))
+        super(AmazonReviewsSpider, self).__init__(*args, **kwargs)
+
+    # Defining a Scrapy parser
+    def parse(self, response):
+        data = response.css('#cm_cr-review_list')
+             
+        # Collecting product star ratings
+        star_rating = data.css('.review-rating')
+             
+        # Collecting user reviews
+        comments = data.css('.review-text')
+        count = 0
+             
+        # Combining the results
+        for review in star_rating:
+            self.results.append({
+                'stars': ''.join(review.xpath('.//text()').extract()),
+                'comment': ''.join(comments[count].xpath(".//text()").extract())
+                })
+            count=count+1
--- skeleton_app/app.py
+++ skeleton_app/app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from amazonReviewsSpider import AmazonReviewsSpider
+
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+spiders = ['amazonReviewsSpider']
+results = []
+@app.route('/reviews/')
+def get_data():
+        id = request.args.get('id')
+        runner = CrawlerRunner()
+        d = runner.crawl(AmazonReviewsSpider, id = id, results = results)
+        d.addBoth(lambda _: reactor.stop())
+        reactor.run()
+        print(results)
+        return(json.dumps(results))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
