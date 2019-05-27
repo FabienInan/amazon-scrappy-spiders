8c1fbd49419bc64e6b601cda67a59e2ab4470a90
Fabien Inan <fabien.inan@gmail.com>
2019-05-26 18:27:27 -0700

    update

--- skeleton_app/app.py
+++ skeleton_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
-from .amazonReviewsSpider import AmazonReviewsSpider
+from amazonReviewsSpider import AmazonReviewsSpider
 
 import json
 
@@ -17,7 +17,6 @@ def get_data():
         d = runner.crawl(AmazonReviewsSpider, id = id, results = results)
         d.addBoth(lambda _: reactor.stop())
         reactor.run()
-        print(results)
         return(json.dumps(results))
 
 if __name__ == '__main__':
